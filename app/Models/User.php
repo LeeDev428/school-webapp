e3374d0ca422da11c169c482ed36afb78a720226
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-27 17:05:57 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -22,6 +23,14 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'usn',
+        'grade',
+        'section',
+        'profile_photo_path',
+        'emergency_contact_relationship',
+        'emergency_contact_email',
+        'emergency_contact_phone',
     ];
 
     /**
@@ -48,5 +57,81 @@ class User extends Authenticatable
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
         ];
+    }
+
+    /**
+     * Check if the user has a specific role.
+     */
+    public function hasRole(string $role): bool
+    {
+        return $this->role === $role;
+    }
+
+    /**
+     * Check if the user is an admin.
+     */
+    public function isAdmin(): bool
+    {
+        return $this->hasRole('admin');
+    }
+
+    /**
+     * Check if the user is a moderator.
+     */
+    public function isModerator(): bool
+    {
+        return $this->hasRole('moderator');
+    }
+
+    /**
+     * Check if the user is a student.
+     */
+    public function isStudent(): bool
+    {
+        return $this->hasRole('student');
+    }
+
+    /**
+     * Get the groups the user belongs to.
+     */
+    public function groups(): BelongsToMany
+    {
+        return $this->belongsToMany(Group::class)->withTimestamps();
+    }
+
+    /**
+     * Get the groups the user moderates (teacher).
+     */
+    public function moderatedGroups(): HasMany
+    {
+        return $this->hasMany(Group::class, 'moderator_id');
+    }
+
+    /**
+     * Get the posts created by the user.
+     */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    /**
+     * Get the comments created by the user.
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * Get all groups accessible to this user (member + moderated).
+     */
+    public function allGroups(): \Illuminate\Database\Eloquent\Collection
+    {
+        if ($this->isAdmin()) {
+            return Group::all();
+        }
+
+        return $this->groups->merge($this->moderatedGroups);
     }
 }
