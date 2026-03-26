c751526b5e5ffa06445e1aa2c7c93303c15beaea
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-27 00:51:07 +0800

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,18 +17,16 @@ class DashboardController extends Controller
         $user = $request->user();
         $groupIds = $user->allGroups()->pluck('id');
 
-        $posts = Post::with(['user:id,name,role,grade,section,profile_photo_path', 'group:id,name', 'comments.user:id,name,profile_photo_path', 'reactions'])
+        $posts = Post::with(['user:id,name,role,grade,section,profile_photo_path', 'group:id,name,moderator_id', 'comments.user:id,name,profile_photo_path', 'reactions'])
             ->visible()
-            ->where(function ($query) use ($groupIds) {
-                $query->whereIn('group_id', $groupIds)
-                    ->orWhere('is_announcement', true);
-            })
+            ->whereIn('group_id', $groupIds)
             ->latest()
             ->paginate(10);
 
         $announcements = Post::with(['user:id,name,role'])
             ->visible()
             ->announcements()
+            ->whereIn('group_id', $groupIds)
             ->latest()
             ->take(5)
             ->get();
